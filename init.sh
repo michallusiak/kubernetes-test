34626e8723ec68c74d283681e7c7502a012658d9
Michal Lusiak <michal.lusiak@siili.com>
2023-12-11 22:59:14 +0100

    mod: init.sh

--- init.sh
+++ init.sh
@@ -4,18 +4,18 @@ set_hostname() {
     local ec2_host_name="minikube"
 
     if [ "$(hostnamectl --static)" != "$ec2_host_name" ]; then
-        if [[ $EUID -ne 0 ]]; then
-            echo -e "\u274C  The initial execution of this script requires root privileges.\n\tPlease run it with elevated permissions."
-            exit 1
-        else
+        # if [[ $EUID -ne 0 ]]; then
+        #     echo -e "\u274C  The initial execution of this script requires root privileges.\n\tPlease run it with elevated permissions."
+        #     exit 1
+        # else
             echo -e "\u2705  Setting up the host name..."
             hostnamectl set-hostname "$ec2_host_name"
-        fi
-    else
-        install_cloud9_cli
-        setup_minikube
+        # fi
+    # else
+    #     install_cloud9_cli
+    #     setup_minikube
 
-        exit 0
+    #     exit 0
     fi
 }
 
@@ -25,24 +25,24 @@ install_packages() {
     # Check whether the OS is Amazon Linux.
     if [ "$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release | sed -e 's/^"//' -e 's/"$//')" == "amzn" ]; then
         echo -e "\u2705  Updating the system packages..."
-        yum -y update
+        sudo yum -y update
 
         for package in "${packages[@]}"; do
             if [ -x $(command -v "$package") ]; then
                 echo -e "\n\u2705  Installing: $package...\n"
-                yum -y install "$package"
+                sudo yum -y install "$package"
 
-                case $package in
-                    "docker")
-                        # Initiate Docker and grant permissions to the 'docker' group.
-                        if ! systemctl start docker || ! systemctl enable docker; then
-                            echo -e "Error:\tFailed to start or enable Docker."
-                            exit 1
-                        else
-                            usermod -aG docker ec2-user
-                        fi
-                        ;;
-                esac
+                # case $package in
+                #     "docker")
+                #         # Initiate Docker and grant permissions to the 'docker' group.
+                #         if ! systemctl start docker || ! systemctl enable docker; then
+                #             echo -e "Error:\tFailed to start or enable Docker."
+                #             exit 1
+                #         else
+                #             usermod -aG docker ec2-user
+                #         fi
+                #         ;;
+                # esac
             fi
         done
     else
@@ -57,13 +57,13 @@ install_cloud9_cli() {
     echo -e "\n\u2705  Installing the Cloud9 CLI...\n"
 
     # Install the Cloud9 CLI: https://cloud9-sdk.readme.io/docs/the-cloud9-cli
-    /home/ec2-user/.nvm/versions/node/v18.17.1/bin/npm install -g c9
+    npm install -g c9
 
     echo -e "\n\u2705  Setting up the IDE...\n"
-    /home/ec2-user/.nvm/versions/node/v18.17.1/bin/c9 open "$doc_dir/pod.yaml"
-    /home/ec2-user/.nvm/versions/node/v18.17.1/bin/c9 open "$doc_dir/node.yaml"
-    /home/ec2-user/.nvm/versions/node/v18.17.1/bin/c9 open "$doc_dir/cluster.yaml"
-    /home/ec2-user/.nvm/versions/node/v18.17.1/bin/c9 open "$doc_dir/minikube.yaml"
+    c9 open "$doc_dir/pod.yaml"
+    c9 open "$doc_dir/node.yaml"
+    c9 open "$doc_dir/cluster.yaml"
+    c9 open "$doc_dir/minikube.yaml"
 }
 
 install_minikube() {
@@ -72,22 +72,24 @@ install_minikube() {
         echo -e "\nMinikube not found.\n\n\u2705  Installing...\n"
 
         # Download and install the most recent version of Minikube.
-        curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube \
-            && chmod +x /usr/local/bin/minikube
+        sudo curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube \
+            && sudo chmod +x /usr/local/bin/minikube
 
         # Create a symbolic link to minikube's binary named 'kubectl'.
-        ln -s -f /usr/local/bin/minikube /usr/local/bin/kubectl
+        sudo ln -s -f /usr/local/bin/minikube /usr/local/bin/kubectl
 
         # echo -e "\nFinalizing the setup is almost complete. Simply press CTRL+D to finish."
         # newgrp docker
 
         echo -e "\n\u2705  Setup complete! Minikube is now ready.\n"
-        # echo -e " Please execute: bash ./Kubernetes/init.sh"
 
-        install_cloud9_cli
-        setup_minikube
-
-        # bash /home/ec2-user/environment/Kubernetes/init.sh
+        if [[ $EUID -ne 0 ]]; then
+            install_cloud9_cli
+            setup_minikube
+        else
+            echo -e " Please execute: bash ./Kubernetes/init.sh"
+            exit 0
+        fi
     fi
 }
 
@@ -125,5 +127,6 @@ setup_minikube() {
 }
 
 set_hostname
-install_packages "docker" "conntrack" "git" "tmux" "jq"
+install_packages "conntrack" "git" "tmux" "jq"
+# install_packages "docker" "conntrack" "git" "tmux" "jq"
 install_minikube
